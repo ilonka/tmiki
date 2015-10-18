a3889b7c495745633de8a47969fc8a0fbcc25ee1
ilonka <ilona.szydlo@o2.pl>
2015-10-18 19:23:00 +0200

    dodanie plikow

---
+++ tmiki/Bernoulli.java
@@ -0,0 +1,37 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class Bernoulli {
+
+	public static int getBinomial(int n, double p) throws IOException {
+		  int x = 0;
+		  for(int i = 0; i < n; i++) {
+		    if(Math.random() < p)
+		      x++;
+		    zapis(i,x);
+		  }
+		  
+			
+		  return x;
+		}
+public static void zapis(int i, int x) throws IOException{ 
+    FileWriter tworzeniePliku = new FileWriter("DANE.csv",true);
+	BufferedWriter zapis = new BufferedWriter(tworzeniePliku);
+	String px = Long.toString(x);
+	String pi = Long.toString(i);
+
+try {
+		zapis.write(pi+";"+px);
+			zapis.newLine(); 
+		}
+	finally{
+				zapis.close();
+			}
+}
+public static void main (String args[]) throws IOException {
+	getBinomial(50, 0.25);
+
+}
+}
